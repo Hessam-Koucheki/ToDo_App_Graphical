e2b250b3fe51ecb80bc90b938a9dae7064c523a1
Hessam <hessam2koucheki@gmail.com>
2021-01-09 21:30:48 +0330

    Fixed Reapetedly reads from file

--- src/read_file.cpp
+++ src/read_file.cpp
@@ -13,6 +13,19 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+// A function to avoid re-read contents from file
+// Actully it controls if a task has been added to vector 
+// and prevents adding it again.
+bool search(const string t ,const vector<Task> & v ){
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        if ( t == v[i].get_task() )
+            return true;
+    }
+    return false;
+}
+
+
 void read_file(vector<Task> & vec)
 {
     ifstream in("../db.dat", std::ios::in);
@@ -23,14 +36,17 @@ void read_file(vector<Task> & vec)
     while (!in.eof())
     {
         getline(in, task, ';');
-        tmp_task.set_task(task);
-        in >> fav >> state;
-        tmp_task.set_favourite(fav);
-        tmp_task.set_state(state);
-        vec.push_back(tmp_task);
-        in.ignore();
+        if ( search(task, vec) == false ){
+            tmp_task.set_task(task);
+            in >> fav >> state;
+            tmp_task.set_favourite(fav);
+            tmp_task.set_state(state);
+            vec.push_back(tmp_task);
+            in.ignore();
+        } else {
+            in.ignore(4);  // Ignore :      ; int int '\n 
+        }
     }
-    
     in.clear();
     in.close();
 }
