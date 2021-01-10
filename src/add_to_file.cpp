5e8604c8b6d1f7fffab1581a427a4da8f397f4e3
Hessam <hessam2koucheki@gmail.com>
2021-01-10 18:42:30 +0330

    FIX: write task several times when app runs onfile

--- src/add_to_file.cpp
+++ src/add_to_file.cpp
@@ -8,15 +8,15 @@ using std::vector;
 
 void add_to_file(vector<Task> &vec)
 {
-    std::ofstream out("../db.dat", std::ios::out | std::ios::app);
+    std::ofstream out("../db.dat", std::ios::out);
     if (!out.is_open())
     {
-        out.open("../db.dat", std::ios::out | std::ios::app);
+        out.open("../db.dat", std::ios::out);
     }
     else
     {
         out.close();
-        out.open("../db.dat", std::ios::out | std::ios::app);
+        out.open("../db.dat", std::ios::out);
     }
     for (auto const &task : vec)
         out << task.get_task() << ";" << task.get_favourite() << ' ' << task.get_state() << endl;
