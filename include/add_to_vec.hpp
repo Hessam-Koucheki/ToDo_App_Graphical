a63a5157f5a20163b45b8b37df59fe0a28072a77
Hessam <hessam2koucheki@gmail.com>
2021-01-10 18:38:11 +0330

    Add ADD_TO_VEC function

---
+++ include/add_to_vec.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include "task.hpp"
+#include <vector>
+void add_to_vec(const Task &, std::vector<Task> &);
---
+++ src/add_to_vec.cpp
@@ -0,0 +1,7 @@
+#include "../include/add_to_vec.hpp"
+#include "../include/task.hpp"
+
+void add_to_vec(const Task &, std::vector<Task> &)
+{
+    
+}
